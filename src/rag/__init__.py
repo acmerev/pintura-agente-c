1b0f07f492b3e851ec980f8f8a9ac9f24863897d
Emmanuel Montiel Cruz <acmerev@gmail.com>
2026-07-23 11:45:07 -0600

    Fix sqlite3 import issue on Oracle Linux
    Replace sqlite3 with pysqlite3 for compatibility on Oracle Linux.

--- src/rag/__init__.py
+++ src/rag/__init__.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')  """  Corrige el problema con sql3 en Oracle Linux """
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from .retriever import PaintRetriever
 
 __all__ = ["PaintRetriever"]
